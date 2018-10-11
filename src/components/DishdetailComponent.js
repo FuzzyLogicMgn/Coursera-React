8d38fa9663ae7a08313b7242c59e59a85b069c9b
Nick <npopov1992@gmail.com>
2018-10-11 22:09:54 +0500

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,70 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
 
-    renderDish(dish) {
-        if (dish != null) {
-            return (
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        } else {
-            return (<div></div>);
-        }
+function RenderDish(dish) {
+    if (dish != null) {
+        return (
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    } else {
+        return (<div></div>);
     }
+}
 
-    renderComments(comments) {
-        if (comments != null) {
-            const dateOpts = {
-                year: 'numeric',
-                month: 'short',
-                day: '2-digit'
-            };
-            const dateFormatter = new Intl.DateTimeFormat("en-US", dateOpts);
-            const commentsItem = comments.map((comm) => {
-                return (
-                    <li>
-                        <p>{comm.comment}</p>
-                        <p>--{comm.author}, {dateFormatter.format(new Date(comm.date))}</p>
-                    </li>
-                );
-            });
+function RenderComments(comments) {
+    if (comments != null) {
+        const dateOpts = {
+            year: 'numeric',
+            month: 'short',
+            day: '2-digit'
+        };
+        const dateFormatter = new Intl.DateTimeFormat("en-US", dateOpts);
+        const commentsItem = comments.map((comm) => {
             return (
-                <div>
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        {commentsItem}
-                    </ul>
-                </div>
+                <li>
+                    <p>{comm.comment}</p>
+                    <p>--{comm.author}, {dateFormatter.format(new Date(comm.date))}</p>
+                </li>
             );
-        } else {
-            return (<div></div>);
-        }
+        });
+        return (
+            <div>
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {commentsItem}
+                </ul>
+            </div>
+        );
+    } else {
+        return (<div></div>);
     }
+}
 
-    render() {
-        if (this.props.selectedDish != null) {
-            return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.selectedDish)}
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            {this.renderComments(this.props.selectedDish.comments)}
-                        </div>
+const DishDetail = (props) => {
+    if (props.selectedDish != null) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {RenderDish(props.selectedDish)}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {RenderComments(props.selectedDish.comments)}
                     </div>
                 </div>
-            );
-        } else {
-            return (<div></div>);
-        }
+            </div>
+        );
+    } else {
+        return (<div></div>);
     }
 }
 
