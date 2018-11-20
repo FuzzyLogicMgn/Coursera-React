310179e17ef733c11a8123860fc6adb1c36c3071
Nick <npopov1992@gmail.com>
2018-11-20 23:35:40 +0500

    Fetch Handling Errors

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -5,10 +5,12 @@ import { baseUrl } from '../shared/baseUrl';
 
 export const fetchComments = () => (dispatch) => {    
     return fetch(baseUrl + 'comments')
-        .then(rs => rs.json())
-        .then(comments => dispatch(addComments(comments)));
+        .then(handleRs, handleErr)
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
 };
 export const addComments = (comments) => ( { type: ActionTypes.ADD_COMMENTS, payload: comments } );
+export const commentsFailed = (errMess) => ( { type: ActionTypes.COMMENTS_FAILED, payload: errMess } );
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: {
@@ -24,8 +26,9 @@ export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
 
     return fetch(baseUrl + 'dishes')
-        .then(rs => rs.json())
-        .then(dishes => dispatch(dishesAdd(dishes)));
+        .then(handleRs, handleErr)
+        .then(dishes => dispatch(dishesAdd(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message)));
 };
 export const dishesLoading = () => ( { type: ActionTypes.DISHES_LOADING } );
 export const dishesFailed = (errMess) => ( { type: ActionTypes.DISHES_FAILED, payload: errMess } );
@@ -36,9 +39,25 @@ export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading(true));
 
     return fetch(baseUrl + 'promotions')
-        .then(rs => rs.json())
-        .then(promos => dispatch(promosAdd(promos)));
+        .then(handleRs, handleErr)
+        .then(promos => dispatch(promosAdd(promos)))
+        .catch(error => dispatch(promosFailed(error.message)));
 };
 export const promosLoading = () => ( { type: ActionTypes.PROMO_LOADING } );
 export const promosFailed = (errMess) => ( { type: ActionTypes.PROMO_FAILED, payload: errMess } );
 export const promosAdd = (promos) => ( { type: ActionTypes.ADD_PROMO, payload: promos } );
+
+
+function handleRs(response) {
+    if (response.ok) {
+        return response.json();
+    } else {
+        const error = new Error('Error: ' + response.status + ' - ' + response.statusText);
+        error.response = response;
+        throw error;
+    }
+};
+
+function handleErr(err) {
+    throw new Error(err.message);
+};
